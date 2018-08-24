baf66aec5fd8afe8136c88a5e2758e44c095bcf9
Xero32 <becker.12@web.de>
2018-08-24 15:54:54 +0200

    Add files via upload

---
+++ MDmain.py
@@ -0,0 +1,31 @@
+#Structure of mdeval.py
+
+Open files
+    Scaling
+    Bounce histogram
+Angular Distribution
+Sticking Coeff
+Plot Populations
+Compress Populations, Transition Populations
+Plot Transition Populations
+Calc Transition Rates
+Compress Transition Rates
+Smooth Transition Rates (via Plot function)
+
+Calculate Analytical Solution
+    Calc Residence Time
+
+    Calc Average Rates
+
+Plot Transition Rates
+
+Plot Solution
+
+Calc Energy Distributions
+Plot Energy Distributions
+
+Plot Kin Energy mean over Bounce number
+Loss Fct to find equilibration time
+
+Plot Trajectory Decrease
+Plot Time Between Bounces
